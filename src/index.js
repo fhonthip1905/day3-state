9856782998a421fd12e9b2ec69adb0291bfc3957
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-04-29 17:13:22 +0700

    Event-Lab-1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import ReactDOM from "react-dom/client";
+import React from "react";
 
+//## Lab-1
 function App() {
-  return (
-    <div className="App">
-      <p>Hello React</p>
-    </div>
-  );
+  const handleChange = (event) => console.log(event.target.value);
+  return <input onChange={handleChange} />;
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

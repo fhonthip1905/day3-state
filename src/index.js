03ce9bcd336065c13f5ed62198536ead74e4cd0d
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-04-29 20:19:49 +0700

    Event-Lab-2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 
 //## Lab-1
+// function App() {
+//   const handleChange = (event) => console.log(event.target.value);
+//   return <input onChange={handleChange} />;
+// }
+
+//## Lab-2
 function App() {
-  const handleChange = (event) => console.log(event.target.value);
-  return <input onChange={handleChange} />;
+  const handleSelect = (event) => console.log(event.target.value);
+  return (
+    <select onClick={handleSelect}>
+      <option>Thailand</option>
+      <option>England</option>
+      <option>Argentina</option>
+      <option>Portugal</option>
+      <option>Spain</option>
+    </select>
+  );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

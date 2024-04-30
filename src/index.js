14cb8ccfb3d065c19d050fc9694b6609e20e1293
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-04-30 09:05:49 +0700

    Event-Lab-4

--- src/index.js
+++ src/index.js
@@ -22,38 +22,56 @@ import React from "react";
 // }
 
 //## Lab-3
+// function App() {
+//   const handleCheckbox = (event) =>
+//     event.target.checked === true
+//       ? console.log('name:'+(event.target.name),'value:'+(event.target.value),'Status:'+(event.target = "Tick"))
+//       : console.log('name:'+(event.target.name),'value:'+(event.target.value),'Status:'+(event.target = "Untick"));
+//   return (
+//     <form>
+//       <input
+//         type="checkbox"
+//         name="bike"
+//         value="bike"
+//         onChange={handleCheckbox}
+//       />
+//       <label> Bike</label>
+//       <br />
+//       <input
+//         type="checkbox"
+//         name="car"
+//         value="car"
+//         onClick={handleCheckbox}
+//       />
+//       <label> Car</label>
+//       <br />
+//       <input
+//         type="checkbox"
+//         name="boat"
+//         value="boat"
+//         onClick={handleCheckbox}
+//       />
+//       <label> Boat</label>
+//       <br />
+//     </form>
+//   );
+// }
+
+//## Lab-4
+
 function App() {
-  const handleCheckbox = (event) =>
-    event.target.checked === true
-      ? console.log('name:'+(event.target.name),'value:'+(event.target.value),'Status:'+(event.target = "Tick"))
-      : console.log('name:'+(event.target.name),'value:'+(event.target.value),'Status:'+(event.target = "Untick"));
+  const handleClick = (event) => {
+    let isConfirm = window.confirm("Leave for Google");
+    if (isConfirm === false) {
+      event.preventDefault();
+    }
+  };
   return (
-    <form>
-      <input
-        type="checkbox"
-        name="bike"
-        value="bike"
-        onChange={handleCheckbox}
-      />
-      <label> Bike</label>
-      <br />
-      <input
-        type="checkbox"
-        name="car"
-        value="car"
-        onClick={handleCheckbox}
-      />
-      <label> Car</label>
-      <br />
-      <input
-        type="checkbox"
-        name="boat"
-        value="boat"
-        onClick={handleCheckbox}
-      />
-      <label> Boat</label>
-      <br />
-    </form>
+    <div>
+      <a href="https://www.google.com/?hl=th" onClick={handleClick}>
+        Go to Google
+      </a>
+    </div>
   );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));

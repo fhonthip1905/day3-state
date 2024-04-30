3f5ccd90846f580595964812fbd6a3f679bfb204
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-04-30 09:59:37 +0700

    State-Lab-1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import ReactDOM from "react-dom/client";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello React</p>
-    </div>
-  );
+  const [isShow, setIsshow] = React.useState(true);
+
+  const handleClick = (event) => {
+    setIsshow(false);
+  };
+
+  // const isShow = true;
+  return
+  <>
+    {isShow ? (
+      <button  onClick={handleClick}>
+        Clck to hide me
+      </button>
+    ) : null}
+  </>;
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

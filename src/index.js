8b82e3a5824531542e6778cf57fd47655da7fb81
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-04-30 10:15:31 +0700

    State-Lab-3

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 
 function App() {
-  const [isShow,setIsshow] = React.useState(true);
-  const handleClick =() =>{
+  const [isShow,setIsshow] = React.useState(false);
 
+  const handleClick =(event) =>{
+    setIsshow(!isShow)
   };
- const isShow = false;
+
+ 
   return (
     <>
-        <button onClick={handleClick} >Click to hide</button>
-        <h2>Text</h2>
+        <button onClick={handleClick}> {isShow ? 'Hide' : 'Show'} </button>
+        {isShow && <h2>Hello</h2> } 
     </>
   
   )
